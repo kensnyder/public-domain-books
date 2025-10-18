3f681f2bd44d582fbb67de08c2c64f7e0ff9d561
Ken Snyder <kendsnyder@gmail.com>
2025-10-17 23:39:44 -0600

    Quad data!

--- src/import-quad.ts
+++ src/import-quad.ts
@@ -4,11 +4,11 @@ main().catch(console.error);
 
 // biome-ignore format: be compact
 const works = {
-  'The Old Testament': { file: '../data/verses/old-testament.json', workOsisID: "kjv", workName: 'Bible', data: [] as OurData[] },
-  'The New Testament': { file: '../data/verses/new-testament.json', workOsisID: "kjv", workName: 'Bible', data: [] as OurData[] },
-  'The Doctrine and Covenants': { file: '../data/verses/d-and-c.json', workOsisID: "D&C", workName: 'Doctrine & Covenants', data: [] as OurData[] },
-  'The Book of Mormon': { file: '../data/verses/bom.json', workOsisID: "BofM", workName: 'Book of Mormon', data: [] as OurData[] },
-  'The Pearl of Great Price': { file: '../data/verses/pgp.json', workOsisID: "PGP", workName: 'Pearl of Great Price', data: [] as OurData[] },
+  'The Old Testament': { file: `${import.meta.dir}/../data/verses/old-testament.json`, workOsisID: "kjv", workName: 'Bible', data: [] as OurData[] },
+  'The New Testament': { file: `${import.meta.dir}/../data/verses/new-testament.json`, workOsisID: "kjv", workName: 'Bible', data: [] as OurData[] },
+  'The Doctrine and Covenants': { file: `${import.meta.dir}/../data/verses/d-and-c.json`, workOsisID: "D&C", workName: 'Doctrine & Covenants', data: [] as OurData[] },
+  'The Book of Mormon': { file: `${import.meta.dir}/../data/verses/bom.json`, workOsisID: "BofM", workName: 'Book of Mormon', data: [] as OurData[] },
+  'The Pearl of Great Price': { file: `${import.meta.dir}/../data/verses/pgp.json`, workOsisID: "PGP", workName: 'Pearl of Great Price', data: [] as OurData[] },
 };
 
 const ymd = new Date().toISOString().slice(0, 10);
@@ -40,6 +40,7 @@ type OurData = {
 }
 
 async function main() {
+  const start = Date.now();
   console.log('Fetching data from GitHub');
   const res = await fetch(
     'https://raw.githubusercontent.com/mormon-documentation-project/lds-scriptures/8c9ae85b3363d7154aa96b6cde24de0b2ef8b1f1/lds-scriptures.json',
@@ -62,9 +63,15 @@ async function main() {
     }
     const work = works[workLongTitle as keyof typeof works];
     if (workLongTitle !== lastWork) {
-      verseSequence = 1;
-      await Bun.file(work.file).write(JSON.stringify(work.data));
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      if (lastWork) {
+        verseSequence = 1;
+        // @ts-expect-error We know works[lastWork] will always be an object
+        const finishedWork = works[lastWork];
+        await Bun.file(finishedWork.file).write(JSON.stringify(finishedWork.data, null, 2));
+        console.log(`Wrote ${lastWork} to ${finishedWork.file.split('/').pop()}`)
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      }
+      console.log(`Starting ${workLongTitle}...`);
     }
     lastWork = workLongTitle;
     const workOsisID = work.workOsisID;
@@ -101,7 +108,8 @@ async function main() {
 
     verseSequence++;
   }
-  console.log('Done writing data to ../data/verses/*.json')
+  console.log('Done writing data to ../data/verses/*.json');
+  console.log(`Took ${Date.now() - start}ms`);
 }
 
 const bookOsisCache = new Map<string, (typeof metadata)[number]>();
