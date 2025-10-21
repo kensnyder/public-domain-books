0e5e43d703307e5a0af7a8b747e891514de9ea72
Ken Snyder <kendsnyder@gmail.com>
2025-10-21 14:58:59 -0600

    rename to work osis ids

--- src/import/generate-manifests.ts
+++ src/import/generate-manifests.ts
@@ -1,26 +1,42 @@
 import { Glob } from 'bun'
 import path from 'path';
 
+type ManifestFormat = {
+  dataPath: string;
+  workOsisID: string;
+  chapterOsisID: string;
+  modelId: string;
+  createdAt: string;
+}
+
 main().catch(console.error);
 async function main() {
   await generateChapterManifest();
 }
 async function generateChapterManifest() {
   const glob = new Glob('**/*.json');
-  const chapterPaths : string[] = [];
-  for await (const filepath of glob.scan(`${import.meta.dir}/../../data/chapters`)) {
-    if (filepath === 'chapter-manifest.json') {
+  const baseDir = `${import.meta.dir}/../../data/chapters`;
+  const manifest : ManifestFormat[] = [];
+  for await (const dataPath of glob.scan(baseDir)) {
+    if (dataPath === 'chapter-manifest.json') {
       continue;
     }
-    chapterPaths.push(filepath);
+    const dataFile = Bun.file(`${baseDir}/${dataPath}`);
+    const dataContents = await dataFile.json();
+    const createdAt = new Date(dataContents.createdAt);
+    manifest.push({
+      dataPath,
+      workOsisID: dataPath.split('/').shift() || '',
+      chapterOsisID: path.basename(dataPath).replace('.json',''),
+      modelId: dataContents.modelId,
+      createdAt: createdAt.toISOString(),
+    });
   }
-  const sorted = chapterPaths.toSorted((a, b) =>
-    a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' })
-  );
-  const manifest = sorted.map(dataPath => ({
-    dataPath,
-    chapterOsisID: path.basename(dataPath).replace('.json',''),
-  }));
+   manifest.sort((a, b) => {
+    const aWorkChapter = `${a.workOsisID}.${a.chapterOsisID}`;
+    const bWorkChapter = `${b.workOsisID}.${b.chapterOsisID}`;
+    return aWorkChapter.localeCompare(bWorkChapter, undefined, { numeric: true, sensitivity: 'base' })
+  });
 
   const file = Bun.file(`${import.meta.dir}/../../data/chapters/chapter-manifest.json`);
   await file.write(JSON.stringify(manifest, null, 2));
